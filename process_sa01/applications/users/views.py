2bb04c13cb86804ceeb97775c04c17f3387aa9d3
cvalenzuela1 <101999993+cvalenzuela1@users.noreply.github.com>
2022-09-07 06:54:36 -0300

    Iniciar sesión creado con forms.py

--- process_sa01/applications/users/views.py
+++ process_sa01/applications/users/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render
 from django.contrib.auth import authenticate, login, logout
 from django.views.generic import ListView
 from django.views.generic.edit import FormView
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
+from django.http import HttpResponseRedirect
 
+from .models import Usuario
 from .forms import LoginForm
 
 # Create your views here.
@@ -13,9 +15,15 @@ class LoginUserView(FormView):
     success_url = reverse_lazy("app_home:home")
 
     def form_valid(self, form):
-        user = authenticate(
-            username=form.cleaned_data['username'],
-            password=form.cleaned_data['password']
-        )
-        login(self.request, user)
-        return super(LoginUserView, self).form_valid(form)
+        username=form.cleaned_data['username']
+        password=form.cleaned_data['password']
+
+        verificacion = Usuario.objects.filter(nombre_usuario=username, password_usuario=password).exists()
+        
+        if verificacion:
+            user = authenticate(username, password)
+            login(self.request, user)
+            return super(LoginUserView, self).form_valid(form)
+        else:
+            return HttpResponseRedirect(reverse("app_users:login"))
+        
