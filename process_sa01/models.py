e435735e9e5bf530dd949429359f9a436b5a7e90
cvalenzuela1 <101999993+cvalenzuela1@users.noreply.github.com>
2022-09-07 05:48:54 -0300

    Migrada base de datos inicial de oracle a DJango
    Usuario Rol Persona Direccion Comuna Ciudad Region

---
+++ process_sa01/models.py
@@ -0,0 +1,221 @@
+# This is an auto-generated Django model module.
+# You'll have to do the following manually to clean this up:
+#   * Rearrange models' order
+#   * Make sure each model has one field with primary_key=True
+#   * Make sure each ForeignKey and OneToOneField has `on_delete` set to the desired behavior
+#   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
+# Feel free to rename the models, but don't rename db_table values or field names.
+from django.db import models
+
+
+class AuthGroup(models.Model):
+    name = models.CharField(unique=True, max_length=150, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_group'
+
+
+class AuthGroupPermissions(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    group = models.ForeignKey(AuthGroup, models.DO_NOTHING)
+    permission = models.ForeignKey('AuthPermission', models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_group_permissions'
+        unique_together = (('group', 'permission'),)
+
+
+class AuthPermission(models.Model):
+    name = models.CharField(max_length=255, blank=True, null=True)
+    content_type = models.ForeignKey('DjangoContentType', models.DO_NOTHING)
+    codename = models.CharField(max_length=100, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_permission'
+        unique_together = (('content_type', 'codename'),)
+
+
+class AuthUser(models.Model):
+    password = models.CharField(max_length=128, blank=True, null=True)
+    last_login = models.DateTimeField(blank=True, null=True)
+    is_superuser = models.BooleanField()
+    username = models.CharField(unique=True, max_length=150, blank=True, null=True)
+    first_name = models.CharField(max_length=150, blank=True, null=True)
+    last_name = models.CharField(max_length=150, blank=True, null=True)
+    email = models.CharField(max_length=254, blank=True, null=True)
+    is_staff = models.BooleanField()
+    is_active = models.BooleanField()
+    date_joined = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'auth_user'
+
+
+class AuthUserGroups(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
+    group = models.ForeignKey(AuthGroup, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_user_groups'
+        unique_together = (('user', 'group'),)
+
+
+class AuthUserUserPermissions(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
+    permission = models.ForeignKey(AuthPermission, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_user_user_permissions'
+        unique_together = (('user', 'permission'),)
+
+
+class Ciudad(models.Model):
+    id_ciudad = models.BigAutoField(primary_key=True)
+    nombre_ciudad = models.CharField(max_length=250)
+    region_id_region = models.ForeignKey('Region', models.DO_NOTHING, db_column='region_id_region')
+
+    class Meta:
+        managed = False
+        db_table = 'ciudad'
+
+
+class Comuna(models.Model):
+    id_comuna = models.BigAutoField(primary_key=True)
+    nombre_comuna = models.CharField(max_length=100)
+    ciudad_id_ciudad = models.ForeignKey(Ciudad, models.DO_NOTHING, db_column='ciudad_id_ciudad')
+
+    class Meta:
+        managed = False
+        db_table = 'comuna'
+
+
+class Direccion(models.Model):
+    id_direccion = models.BigAutoField(primary_key=True)
+    nombre_calle = models.CharField(max_length=250)
+    numero_casa = models.BigIntegerField()
+    comuna_id_comuna = models.ForeignKey(Comuna, models.DO_NOTHING, db_column='comuna_id_comuna')
+
+    class Meta:
+        managed = False
+        db_table = 'direccion'
+
+
+class DjangoAdminLog(models.Model):
+    action_time = models.DateTimeField()
+    object_id = models.TextField(blank=True, null=True)
+    object_repr = models.CharField(max_length=200, blank=True, null=True)
+    action_flag = models.IntegerField()
+    change_message = models.TextField(blank=True, null=True)
+    content_type = models.ForeignKey('DjangoContentType', models.DO_NOTHING, blank=True, null=True)
+    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'django_admin_log'
+
+
+class DjangoContentType(models.Model):
+    app_label = models.CharField(max_length=100, blank=True, null=True)
+    model = models.CharField(max_length=100, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'django_content_type'
+        unique_together = (('app_label', 'model'),)
+
+
+class DjangoMigrations(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    app = models.CharField(max_length=255, blank=True, null=True)
+    name = models.CharField(max_length=255, blank=True, null=True)
+    applied = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'django_migrations'
+
+
+class DjangoSession(models.Model):
+    session_key = models.CharField(primary_key=True, max_length=40)
+    session_data = models.TextField(blank=True, null=True)
+    expire_date = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'django_session'
+
+
+class Persona(models.Model):
+    id_persona = models.BigAutoField(primary_key=True)
+    rut_persona = models.CharField(max_length=10)
+    nombre_persona = models.CharField(max_length=30)
+    apellido_paterno_persona = models.CharField(max_length=30)
+    apellido_materno_persona = models.CharField(max_length=30)
+    direccion_id_direccion = models.ForeignKey(Direccion, models.DO_NOTHING, db_column='direccion_id_direccion')
+
+    class Meta:
+        managed = False
+        db_table = 'persona'
+
+
+class Region(models.Model):
+    id_region = models.BigAutoField(primary_key=True)
+    nombre_region = models.CharField(max_length=250)
+
+    class Meta:
+        managed = False
+        db_table = 'region'
+
+
+class Rol(models.Model):
+    id_rol = models.BigAutoField(primary_key=True)
+    nombre = models.CharField(max_length=50)
+
+    class Meta:
+        managed = False
+        db_table = 'rol'
+
+
+class Tarea(models.Model):
+    id_tarea = models.BigAutoField(primary_key=True)
+    titulo_tarea = models.CharField(max_length=50)
+    desc_tarea = models.CharField(max_length=500)
+    fecha_inicio = models.DateField()
+    fecha_termino = models.DateField()
+    etiqueta = models.CharField(max_length=50)
+    porc_cumplimiento = models.BigIntegerField()
+    estado_tarea = models.CharField(max_length=50)
+
+    class Meta:
+        managed = False
+        db_table = 'tarea'
+
+
+class TareaPersona(models.Model):
+    id_tarea_persona = models.BigAutoField(primary_key=True)
+    persona_id_persona = models.ForeignKey(Persona, models.DO_NOTHING, db_column='persona_id_persona')
+    tarea_id_tarea = models.ForeignKey(Tarea, models.DO_NOTHING, db_column='tarea_id_tarea')
+
+    class Meta:
+        managed = False
+        db_table = 'tarea_persona'
+
+
+class Usuario(models.Model):
+    id_usuario = models.BigAutoField(primary_key=True)
+    nombre_usuario = models.CharField(max_length=50)
+    password_usuario = models.CharField(max_length=50)
+    rol_id_rol = models.ForeignKey(Rol, models.DO_NOTHING, db_column='rol_id_rol')
+    persona_id_persona = models.ForeignKey(Persona, models.DO_NOTHING, db_column='persona_id_persona')
+
+    class Meta:
+        managed = False
+        db_table = 'usuario'
