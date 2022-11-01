29ccc02bfc561387f681fc08f358e2c7ebb8c521
cvalenzuela1 <101999993+cvalenzuela1@users.noreply.github.com>
2022-11-01 18:10:49 -0300

    Carga de trabajo con filtros funcionales - Lista

--- process_sa01/static/js/users/calcular_carga_trabajo.js
+++ process_sa01/static/js/users/calcular_carga_trabajo.js
@@ -82,3 +82,92 @@ function detalleTarea(tarea_id){
         return false;
     }
 }
+// RUT PERSONA FILTROS  //
+// Validar si filtro es númerica y contiene guión para el rut
+function validate(evt) {
+    var theEvent = evt || window.event;
+
+    // Handle paste
+    if (theEvent.type === 'paste') {
+        key = event.clipboardData.getData('text/plain');
+    } else {
+    // Handle key press
+        var key = theEvent.keyCode || theEvent.which;
+        key = String.fromCharCode(key);
+    }
+    var regex = /[0-9]|\-/;
+    if( !regex.test(key) ) {
+        theEvent.returnValue = false;
+        if(theEvent.preventDefault) theEvent.preventDefault();
+    }
+}
+
+function filterRutPersona() {
+    // Declare variables
+    var input, filter, table, tr, td, i, txtValue;
+    input = document.getElementById("filterInputRutPersona");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("tablaTareaPersona");
+    tr = table.getElementsByTagName("tr");
+
+    // Loop through all table rows, and hide those who don't match the search query
+    for (i = 0; i < tr.length; i++) {
+      td = tr[i].getElementsByTagName("td")[0];
+      if (td) {
+        txtValue = td.textContent || td.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+          tr[i].style.display = "";
+        } else {
+          tr[i].style.display = "none";
+        }
+      }
+    }
+}
+
+function btnCleanRutPersonaClick(){
+    // 👇️ if you are submitting a form (prevents page reload)
+    event.preventDefault();
+
+    const filterInputRutPersona = document.getElementById('filterInputRutPersona');
+
+    // 👇️ clear input field
+    filterInputRutPersona.value = '';
+    filterRutPersona();
+}
+// END RUT PERSONA FILTROS  //
+
+// NOMBRE PERSONA FILTROS  //
+function filterNombrePersona() {
+    // Declare variables
+    var input, filter, table, tr, td, i, txtValue;
+    input = document.getElementById("filterInputNombrePersona");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("tablaTareaPersona");
+    tr = table.getElementsByTagName("tr");
+
+    // Loop through all table rows, and hide those who don't match the search query
+    for (i = 0; i < tr.length; i++) {
+        td = tr[i].getElementsByTagName("td")[1];
+        if (td) {
+            txtValue = td.textContent || td.innerText;
+            if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                tr[i].style.display = "";
+            } else {
+                tr[i].style.display = "none";
+            }
+        }
+    }
+}
+
+function btnCleanNombrePersonaClick(){
+    // 👇️ if you are submitting a form (prevents page reload)
+    event.preventDefault();
+
+    const filterInputNombrePersona = document.getElementById('filterInputNombrePersona');
+
+    // 👇️ clear input field
+    filterInputNombrePersona.value = '';
+    filterNombrePersona();
+}
+
+// END NOMBRE PERSONA FILTROS  //
